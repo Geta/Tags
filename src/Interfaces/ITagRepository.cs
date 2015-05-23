162784b8cdea7915956f3c006867c5c18d39b1f1
Frederik Vig <frederik@geta.no>
2015-05-23 12:28:54 -0400

    Cleaned up root directory and put source code into src folder Upgraded to .NET 4.5 and EPiServer 8

---
+++ src/Interfaces/ITagRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EPiServer.Data;
+using Geta.Tags.Models;
+
+namespace Geta.Tags.Interfaces
+{
+    using System;
+
+    public interface ITagRepository
+    {
+        Tag GetTagById(Identity id);
+        Tag GetTagByName(string name);
+        IEnumerable<Tag> GetTagsByPage(Guid pageGuid);
+        IQueryable<Tag> GetAllTags();
+        Identity Save(Tag tag);
+        void Delete(Tag tag); 
+    }
+}
---
+++ src/Interfaces/ITagService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EPiServer.Data;
+using Geta.Tags.Models;
+
+namespace Geta.Tags.Interfaces
+{
+    using System;
+
+    public interface ITagService
+    {
+        Tag GetTagById(Identity id);
+        IEnumerable<Tag> GetTagByPage(Guid pageGuid);
+        Tag GetTagByName(string name);
+        IQueryable<Tag> GetAllTags();
+        Identity Save(Tag tag);
+        Tag Save(Guid pageGuid, string name);
+        void Save(Guid pageGuid, IEnumerable<string> names);
+        void Delete(string name);
+        void Delete(Identity id); 
+    }
+}
