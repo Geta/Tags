277b63ac1bf4d74c5d39120b3fc2eb8944ed0bfc
Josef Ottosson <josef.ottosson@creuna.se>
2016-05-03 11:55:23 +0200

    Fixes caseSensitive always being true

--- src/EditorDescriptors/TagsEditorDescriptor.cs
+++ src/EditorDescriptors/TagsEditorDescriptor.cs
@@ -21,10 +21,12 @@ namespace Geta.Tags.EditorDescriptors
             base.ModifyMetadata(metadata, attributes);
             var groupKeyAttribute = attributes.FirstOrDefault(a => typeof (TagsGroupKeyAttribute) == a.GetType()) as TagsGroupKeyAttribute;
             var cultureSpecificAttribute = attributes.FirstOrDefault(a => typeof(CultureSpecificAttribute) == a.GetType()) as CultureSpecificAttribute;
+            var getaAttribute = attributes.FirstOrDefault(a => typeof(GetaTagsAttribute) == a.GetType()) as GetaTagsAttribute;
+
             metadata.EditorConfiguration["GroupKey"] = Helpers.TagsHelper.GetGroupKeyFromAttributes(groupKeyAttribute, cultureSpecificAttribute);
-            metadata.EditorConfiguration["allowSpaces"] = false;
-            metadata.EditorConfiguration["allowDuplicates"] = false;
-            metadata.EditorConfiguration["caseSensitive"] = true;
+            metadata.EditorConfiguration["allowSpaces"] = getaAttribute != null ? getaAttribute.AllowSpaces : false;
+            metadata.EditorConfiguration["allowDuplicates"] = getaAttribute != null ? getaAttribute.AllowDuplicates : false;
+            metadata.EditorConfiguration["caseSensitive"] = getaAttribute != null ? getaAttribute.CaseSensitive : true;
         }
     }
 }
