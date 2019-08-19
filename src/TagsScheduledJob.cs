d742672c0e15c060520ad9438d879b0728d325c5
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2019-08-19 18:18:18 +0300

    changed license to Apache-2.0

--- src/TagsScheduledJob.cs
+++ src/TagsScheduledJob.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) Geta Digital. All rights reserved.
-// Licensed under MIT. See the LICENSE file in the project root for more information
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
 
 using System;
 using System.Collections.Generic;
